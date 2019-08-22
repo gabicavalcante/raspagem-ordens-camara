b263369e2099af4c852bf72a5414e5c706e0acfd
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-08-22 09:14:44 -0300

    refactor code

--- read_pdf.py
+++ read_pdf.py
@@ -28,40 +28,41 @@ def replace_all(symbol_scape, content):
     return new_str
 
 
-def find_orador(keywords):
-    find_orador = False
-    oradores = []
+def find_orator(keywords):
+    found_orators = False
+    orators = []
     for keyword in keywords:
-        if find_orador and re.search(r'\d\dh', keyword):
-            find_orador = False
-        if find_orador and keyword != '.':
-            oradores.append(keyword)
+        if found_orators and re.search(r'\d\dh', keyword):
+            found_orators = False
+        if found_orators and keyword != '.':
+            orators.append(keyword)
         if keyword == "ORADOR":
-            find_orador = True
-    oradores = [s.split('.') for s in oradores if s not in partidos]
-    oradores = list(itertools.chain.from_iterable(oradores))
-    oradores_final = []
+            found_orators = True
+    orators = [s.split('.') for s in orators if s not in partidos]
+    orators = list(itertools.chain.from_iterable(orators))
+    final_orators = []
 
     count = -1
-    for s in oradores:
+    for s in orators:
         if re.search(r'VER[.ª]*[ª.]*', s):
             count += 1
-            oradores_final.append('')
+            final_orators.append('')
         elif s != 'a':
-            oradores_final[count] += s + ' '
-    return [s.rstrip() for s in oradores_final]
+            final_orators[count] += s + ' '
+    return [s.rstrip() for s in final_orators]
 
 
 def find_topics(keywords):
-    flag_titulo = False
-    flag_responsavel = False
-    flag_assunto = False
-    flag_movimento = False
+    flag_title = False
+    flag_responsible = False
+    flag_subject = False
+    flag_forwarding = False
 
     flag_start_content = False
-    pauta = {'tipo': '', 'n': '', 'responsavel': '',
+    topic = {'tipo': '', 'n': '', 'responsavel': '',
              'movimento': '', 'assunto': ''}
-    list_pautas = []
+
+    list_topics = []
     for keyword in keywords:
         if keyword == 'PAUTA':
             flag_start_content = True
@@ -69,98 +70,101 @@ def find_topics(keywords):
         if not flag_start_content:
             continue
 
-        if keyword == 'ASSUNTO' and flag_titulo and not flag_movimento:
-            pauta['tipo'] = pauta['tipo'].rstrip().replace(' .', '')
+        if keyword == 'ASSUNTO' and flag_title and not flag_forwarding:
+            topic['tipo'] = topic['tipo'].rstrip().replace(' .', '')
             responsavel = re.sub(
                 r'VER[.a]*[.ª]*[ .]* ', '',
-                pauta['responsavel'].rstrip()).replace(' .', '')
+                topic['responsavel'].rstrip()).replace(' .', '')
             # as vezes teremos mais de um partido envolvido
-            #pauta['partido'] = responsavel.rsplit(' ', 1)[1]
-            pauta['responsavel'] = responsavel  # .rsplit(' ', 1)[0]
+            # topic['partido'] = responsavel.rsplit(' ', 1)[1]
+            topic['responsavel'] = responsavel  # .rsplit(' ', 1)[0]
 
-            pauta = {'tipo': '', 'n': '', 'responsavel': '',
+            topic = {'tipo': '', 'n': '', 'responsavel': '',
                      'movimento': '', 'assunto': ''}
 
-            flag_titulo = False
-            flag_assunto = True
+            flag_title = False
+            flag_subject = True
             continue
 
-        if keyword == 'MOVIMENTO' and flag_assunto and not flag_titulo:
-            flag_assunto = False
-            flag_movimento = True
+        if keyword == 'MOVIMENTO' and flag_subject and not flag_title:
+            flag_subject = False
+            flag_forwarding = True
             continue
 
-        if keyword in ['PROJETO', 'REQUERIMENTO', 'MOÇÃO'] and not flag_assunto:
-            if flag_movimento or flag_start_content:
-                if flag_movimento:
-                    pauta['assunto'] = pauta['assunto'].replace(
+        if keyword in ['PROJETO', 'REQUERIMENTO', 'MOÇÃO'] and not flag_subject:
+            if flag_forwarding or flag_start_content:
+                if flag_forwarding:
+                    topic['assunto'] = topic['assunto'].replace(
                         ' . ', '')
-                    pauta['movimento'] = responsavel = re.sub(
+                    topic['movimento'] = responsavel = re.sub(
                         r'ESTADO DO RIO GRANDE DO NORTE CÂMARA MUNICIPAL DO NATAL PALÁCIO PADRE MIGUELINHO \d[\d]*', '',
-                        pauta['movimento'].rstrip()).replace('.', '').rstrip()
-                    list_pautas.append(pauta)
-                flag_titulo = True
-                flag_assunto = flag_movimento = flag_responsavel = False
+                        topic['movimento'].rstrip()).replace('.', '').rstrip()
+                    list_topics.append(topic)
+                flag_title = True
+                flag_subject = flag_forwarding = flag_responsible = False
             else:
                 # quuando a string 'projeto' se repete em motivmento e titulo
                 continue
 
-        if (flag_assunto and not flag_titulo):
-            pauta['assunto'] += keyword + ' '
+        if (flag_subject and not flag_title):
+            topic['assunto'] += keyword + ' '
 
-        if (flag_movimento and not flag_assunto):
-            pauta['movimento'] += keyword + ' '
+        if (flag_forwarding and not flag_subject):
+            topic['movimento'] += keyword + ' '
 
-        if flag_titulo and re.search(r'\d/20\d\d', keyword):
-            pauta['n'] = keyword
-            flag_responsavel = True
-        elif flag_titulo and flag_responsavel:
-            pauta['responsavel'] += keyword + ' '
-        elif flag_titulo and not re.search(r'^N[.]*[\u00ba]+', keyword):
-            pauta['tipo'] += keyword + ' '
+        if flag_title and re.search(r'\d/20\d\d', keyword):
+            topic['n'] = keyword
+            flag_responsible = True
+        elif flag_title and flag_responsible:
+            topic['responsavel'] += keyword + ' '
+        elif flag_title and not re.search(r'^N[.]*[\u00ba]+', keyword):
+            topic['tipo'] += keyword + ' '
 
-    return list_pautas
+    return list_topics
 
 
-pdfFileObj = open('documents/Abril/ordem_do_dia_07_05_19.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-pageObj = pdfReader.getPage(0)
+def read_content(file_path):
+    pdfFileObj = open(file_path, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-pdf_text = ''
-for page in range(pdfReader.numPages):
-    pdf_text += pdfReader.getPage(page).extractText()
-    pdf_text = replace_all(scape, pdf_text)
+    pdf_text = ''
+    for page in range(pdfReader.numPages):
+        pdf_text += pdfReader.getPage(page).extractText()
+        pdf_text = replace_all(scape, pdf_text)
 
-tokens = word_tokenize(pdf_text)
-# stop_words = stopwords.words('portuguese')
-# not word in stop_words and
-keywords = [
-    word for word in tokens if not word in punctuations]
+    tokens = word_tokenize(pdf_text)
+    keywords = [
+        word for word in tokens if not word in punctuations]
 
-documento = {}
-documento['oradores'] = find_orador(keywords[0:150])
-documento['pautas'] = find_topics(keywords)
+    documento = {}
+    documento['oradores'] = find_orator(keywords[0:150])
+    documento['pautas'] = find_topics(keywords)
 
-print(json.dumps(documento, sort_keys=True, indent=4, ensure_ascii=False))
+    return documento
 
-stop_words = stopwords.words('portuguese')
-# content = ' '.join([
-#    word.lower() for word in keywords if not word in stop_words])
-content = ''
-for pauta in documento['pautas']:
-    assunto = ' '.join(
-        [word.lower() for word in pauta['assunto'].split(' ') if not word in stop_words])
-    content += assunto
 
-wordcloud = WordCloud(width=800, height=800,
-                      background_color='white',
-                      stopwords=set(STOPWORDS),
-                      min_font_size=10).generate(content)
+def word_cloud():
+    stop_words = stopwords.words('portuguese')
+    content = ''
+    for pauta in documento['pautas']:
+        assunto = ' '.join(
+            [word.lower() for word in pauta['assunto'].split(' ') if not word in stop_words])
+        content += assunto
 
-# plot the WordCloud image
-plt.figure(figsize=(8, 8), facecolor=None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad=0)
+    wordcloud = WordCloud(width=800, height=800,
+                          background_color='white',
+                          stopwords=set(STOPWORDS),
+                          min_font_size=10).generate(content)
 
-plt.show()
+    # plot the WordCloud image
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    documento = read_content('documents/Abril/ordem_do_dia_07_05_19.pdf')
+    print(json.dumps(documento, sort_keys=True, indent=4, ensure_ascii=False))
