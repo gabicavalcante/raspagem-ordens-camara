d1f4a97789925ba5438e71dc77f2c887bd376374
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-08-22 09:44:41 -0300

    add comments

--- python_scripts/preprocessing.py
+++ python_scripts/preprocessing.py
@@ -22,6 +22,16 @@ scape = ['\n', '\\', '-', 'Œ']
 
 
 def replace_all(symbol_scape, content):
+    """
+    Replace all 'symbol_scape' from given string
+
+    Args: 
+    symbol_scape : document words 
+    content: string to remove symbols
+
+    Returns:
+        string without symbols 
+    """
     new_str = content
     for symbol in symbol_scape:
         new_str = new_str.replace(symbol, '')
@@ -29,13 +39,25 @@ def replace_all(symbol_scape, content):
 
 
 def find_orator(keywords):
+    """
+    Return orators
+
+    Args: 
+    keywords : document words 
+
+    Returns:
+        a list of orators name 
+    """
     found_orators = False
     orators = []
     for keyword in keywords:
+        # found somethig like 14h/14h15
         if found_orators and re.search(r'\d\dh', keyword):
             found_orators = False
+        # append orator name if not
         if found_orators and keyword != '.':
             orators.append(keyword)
+        # found a new orator
         if keyword == "ORADOR":
             found_orators = True
     orators = [s.split('.') for s in orators if s not in partidos]
@@ -43,12 +65,15 @@ def find_orator(keywords):
     final_orators = []
 
     count = -1
+
+    # append string to find orators names
     for s in orators:
         if re.search(r'VER[.ª]*[ª.]*', s):
             count += 1
             final_orators.append('')
-        elif s != 'a':
+        elif s not in ['a', 'ª']:
             final_orators[count] += s + ' '
+
     return [s.rstrip() for s in final_orators]
 
 
@@ -79,9 +104,6 @@ def find_topics(keywords):
             # topic['partido'] = responsavel.rsplit(' ', 1)[1]
             topic['responsavel'] = responsavel  # .rsplit(' ', 1)[0]
 
-            topic = {'tipo': '', 'n': '', 'responsavel': '',
-                     'movimento': '', 'assunto': ''}
-
             flag_title = False
             flag_subject = True
             continue
@@ -100,6 +122,10 @@ def find_topics(keywords):
                         r'ESTADO DO RIO GRANDE DO NORTE CÂMARA MUNICIPAL DO NATAL PALÁCIO PADRE MIGUELINHO \d[\d]*', '',
                         topic['movimento'].rstrip()).replace('.', '').rstrip()
                     list_topics.append(topic)
+
+                    topic = {'tipo': '', 'n': '', 'responsavel': '',
+                             'movimento': '', 'assunto': ''}
+
                 flag_title = True
                 flag_subject = flag_forwarding = flag_responsible = False
             else:
@@ -166,5 +192,5 @@ def word_cloud():
 
 
 if __name__ == "__main__":
-    documento = read_content('documents/Abril/ordem_do_dia_07_05_19.pdf')
+    documento = read_content('../documents/Abril/ordem_do_dia_07_05_19.pdf')
     print(json.dumps(documento, sort_keys=True, indent=4, ensure_ascii=False))
